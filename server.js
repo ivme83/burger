6d637183cdae35a95ce7249a2a612c9bef2993e1
Roland Willis <rolandwillisiv@gmail.com>
2018-04-29 09:35:46 -0700

    rebuilt package.json

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
